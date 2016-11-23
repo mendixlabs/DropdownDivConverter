32036bbcbd0d01cd86657f7a449a32b24cde6a06
Eric Tieniber <tieniber@gmail.com>
2016-11-23 15:54:47 -0500

    merged changed with base fork, grunt->gulp

--- src/DropdownDivConverter/widget/DropdownDivConverter2.js
+++ src/DropdownDivConverter/widget/DropdownDivConverter2.js
@@ -6,7 +6,7 @@
     ========================
 
     @file      : DropdownDivConverter.js
-    @version   : 1.4
+    @version   : 1.0
     @author    : Willem Gorisse
     @date      : Wed, 24 Jun 2015 15:15:32 GMT
     @copyright : 2015 Mendix
@@ -65,19 +65,19 @@ define([
             this._allDropDowns[this.id] = this;
             this._isOpen = false;
         },
-
+        
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function (obj, callback) {
-            //this._contextObj = obj;
-			var contextChange = this._contextObj !== obj;
+            var contextChange = this._contextObj !== obj;
 			this._contextObj = obj;
+            
             this._resetSubscriptions();
-
+			
 			//child objects don't get their update function called automatically for some reason
 			if(contextChange) {
-				this._updateChildren(obj);
+				this._updateChildren(obj);	
 			}
-
+			
             this._updateRendering(callback);
 
         },
@@ -90,11 +90,11 @@ define([
             dojoArray.forEach(children, lang.hitch(this, function(entry, i) {
                 var widget = dijit.registry.byNode(entry);
 				if (widget && widget.update) {
-					widget.update(obj, function(){});
+					widget.update(obj, function(){});	
 				}
             }));
         },
-
+		
         // Reordering the interface: selecting the siblings and putting them in the dropdown menu
         _updateRendering: function (callback) {
             // find all the siblings
@@ -103,19 +103,20 @@ define([
             dojoArray.forEach(siblings, lang.hitch(this, function(entry, i) {
                 domConstruct.place(entry,this.dropdownMenu,"last");
             }));
-
+            
             this._renderInterface(this.startOpen,callback);
         },
 
         // Really render the interface, if renderAsOpen is true: render the menu in open state
         _renderInterface: function(renderAsOpen, callback) {
             this._updateButtonTitle();
-
+			
+          
             this._setButtonTypes(this.dropdownButton);
-
+            
             // implement visual settings of the widget
             if (renderAsOpen && !domClass.contains(this.domNode,"open")) {
-                domClass.add(this.domNode,"open");
+                domClass.add(this.domNode,"open"); 
                 this._isOpen = true;
             }
             if (this.isDropUp) {
@@ -124,27 +125,27 @@ define([
             if (this.isRightAligned) {
                 if (!domClass.contains(this.dropdownMenu, "dropdown-menu-right")) {
                     domClass.add(this.dropdownMenu, "dropdown-menu-right");
-                }
+                }   
             }
             if (this.splitButtonActive) {
-                this._createSplitButton();
+                this._createSplitButton();  
             } else {
                 domConstruct.destroy(this.splitButton);
             }
-
-            this._setupEvents(callback);
+            
+            this._setupEvents(callback);  
         },
-
-		// Attach events to HTML dom elements
+        
+        // Attach events to HTML dom elements
         _setupEvents: function (callback) {
             // only set events one time
             if (!this._eventsSet) {
                 this._eventsSet = true;
-
+                
                 // set window click
                 this.connect(document, "click", lang.hitch(this,function(event){
                     // if a widget external click is made: close the menu if open
-
+					
 					//if autoclose is enabled, we can just do a simple close here if open
 					if (this.autoClose) {
 						if (domClass.contains(this.domNode,"open")) {
@@ -162,31 +163,31 @@ define([
                 this.connect(this.dropdownButton, "click", lang.hitch(this,function(e){
                     event.stop(e);
                     var dropdown = null;
-
+					
 					/*
 					var parentDropDown = null;
 					var parentNode = domQuery(this.domNode.parentNode).closest(".dropdown-div-converter")[0];
-
+					
                     if (parentNode) {
 						parentDropDown = dijit.registry.byNode(parentNode).id;
 					}
 					*/
-
+					
                     for(dropdown in this._allDropDowns) {
 						//Don't close a parent dropdown just because a child DropdownDivConverter was clicked.
 						if (this._allDropDowns[dropdown].domNode.contains(this.domNode)) continue;
-
-						if (this._allDropDowns.hasOwnProperty(dropdown) && dropdown !== this.id){
+						
+						if (this._allDropDowns.hasOwnProperty(dropdown) && dropdown !== this.id){					
 							if (this._allDropDowns[dropdown]._isOpen === true) {
                                 domClass.remove(this._allDropDowns[dropdown].domNode, "open");
                                 this._allDropDowns[dropdown]._isOpen = false;
                             }
-                        }
+                        }         
                     }
-
+                    
                     this._toggleMenu();
                 }));
-
+                
                 // prevent default closing on dropdownMenu if needed
                 // ET 3/30/2016, replaced stopping the event with a check on the close handler to see if the click happened inside the dropdownmenu
 				// This was done to fix a bug when using a tooltip inside the dropdown
@@ -197,18 +198,18 @@ define([
                     }));
                 }
                 */
-
+				
 				// Handle Mendix buttons and links, and listviews, since their events don't bubble.
                 this._setInternalButtonListeners();
-
+                
                 // set the action for the possible split group button
                 if (this.splitButtonActive){
                     this.connect(this.splitButton, "click", lang.hitch(this, function(e){
-
+                        
                         // if a microflow is checked and a contextobject is defined
                         if (this.splitButtonClicked !== "" && this._contextObj) {
                             // do we have a contextObj for the microflow?
-
+                            
                             var id = this._contextObj.getGuid();
 
                             mx.data.action({
@@ -222,7 +223,7 @@ define([
                                 },
                                 error           : function(error) {
                                     // if there was an error
-                                }
+                                } 
                             }, this);
                         } else if (this.simpleSplitButtonClicked !== "") {
 
@@ -239,7 +240,7 @@ define([
                                 }
                             }, this);
                         }
-
+                        
                     }));
                 }
 
@@ -248,7 +249,7 @@ define([
                 callback();
             }
         },
-
+		
 		_setInternalButtonListeners: function() {
 			if (this._buttonHandles) {
 				dojoArray.forEach(this._buttonHandles, function(handle, i){
@@ -257,10 +258,10 @@ define([
 
 				this._buttonHandles = [];
             }
-
+			
 			// Mendix buttons and links stop events from bubbling: set actions for internal button clicks to close the menu if needed
 			if (this.autoClose){
-				var internalButtons = domQuery("button, a, .mx-listview-clickable .mx-list",this.dropdownMenu);
+				var internalButtons = domQuery("button, a, .mx-listview-item",this.dropdownMenu);
 				dojoArray.forEach(internalButtons, lang.hitch(this,function(node, i){
 					this._buttonHandles[i] = this.connect(node, "click", lang.hitch(this, function(e) {
 						if (this._isOpen){
@@ -270,7 +271,7 @@ define([
 				}));
 			}
 		},
-
+		
 		_getButtonTextFromReference: function() {
 			var ref = this.buttonEntity.split('/')[0],
 			refGuid = this._contextObj.getReference(ref);
@@ -288,14 +289,14 @@ define([
 					}
 				});
 			} else {
-				this._setButtonTitle(this.buttonTitle);
-			}
+				this._setButtonTitle(this.buttonTitle);	
+			}	
 		},
-
+		
 		_updateButtonTitle: function() {
 			var newTitle = this.buttonTitle;
 			var needCallback = false;
-
+			
 			if (this.buttonAttribute !== '' && this._contextObj) {
 				if (this.buttonEntity == this.contextObject) {
 					if (this._contextObj.getEnumCaption(this.buttonAttribute)) {
@@ -308,33 +309,33 @@ define([
 					this._getButtonTextFromReference();
 				}
 			}
-
+			
 			if (!needCallback) {
 				this._setButtonTitle(newTitle);
-			}
+			}		
 		},
-
+							
 		_setButtonTitle: function(titleText) {
-
+		
 			if(this.splitButtonActive) {
 				this.splitButton.innerHTML = titleText;
 				// if a glyphicon icon was requested and the splitButton is not wanted: add the glyphicon to the button.
 				if (this.buttonGlyphicon !== ''){
-					this._addGlyphicon(this.splitButton);
+					this._addGlyphicon(this.splitButton);   
 				}
 			} else {
 				this.dropdownButton.innerHTML = titleText + "<span class='caret'></span>";
 				// if a glyphicon icon was requested and the splitButton is not wanted: add the glyphicon to the button.
 				if (this.buttonGlyphicon !== '') {
-					this._addGlyphicon(this.dropdownButton);
+					this._addGlyphicon(this.dropdownButton);   
 				}
 			}
-
+			
 		},
-
-		// toggle dropdown state
+        
+        // toggle dropdown state
         _toggleMenu: function() {
-
+			
             if (domClass.contains(this.domNode,"open")){
                 domClass.remove(this.domNode,"open");
                 this._isOpen = false;
@@ -343,20 +344,20 @@ define([
                 this._isOpen = true;
 				this._setInternalButtonListeners();
             }
-
+			
 			this._updateButtonTitle();
         },
-
+        
         // Set button types: size and type
         _setButtonTypes: function(button){
             // first set the button type
             var typeClassName = "btn-"+this.buttonType,
                 sizeClassName;
-
+            
             if (!domClass.contains(button,typeClassName)){
                 domClass.add(button,typeClassName);
             }
-
+            
             switch(this.buttonSize) {
                 case "default":
                     // default buttonsize is allready implemented
@@ -365,32 +366,32 @@ define([
                     sizeClassName = "btn-"+this.buttonSize;
                     if (!domClass.contains(button,sizeClassName)){
                         domClass.add(button,sizeClassName);
-                    }
+                    }                    
             }
         },
-
+        
         // Transform the dropdown into a dropup
         _transformToDropUp: function() {
             if (!domClass.contains(this.domNode,"dropup")){
                 domClass.add(this.domNode,"dropup");
             }
         },
-
-        // Create a split button group
+        
+        // Create a split button group 
         _createSplitButton: function() {
             this._setButtonTypes(this.splitButton);
-
+            
             // adjust the dropdownButtons content
             this.dropdownButton.innerHTML = "<span class='caret'></span><span class='sr-only'>Toggle Dropdown</span";
         },
-
+        
         // Add a glyphicon to a button
         _addGlyphicon: function(buttonObject) {
             buttonObject.innerHTML = "<span class='glyphicon " + this.buttonGlyphicon + "'></span>" + buttonObject.innerHTML;
         },
 
         // Reset subscriptions.
-		_resetSubscriptions: function () {
+        _resetSubscriptions: function () {
             var _objectHandle = null,
                 _attrHandle = null,
 				_contextHandle = null,
@@ -405,35 +406,35 @@ define([
                 this._handles = [];
             }
 
-            // When a mendix object exists create subscribtions.
+            // When a mendix object exists create subscribtions. 
             if (this._contextObj && this.buttonEntity && this.buttonAttribute) {
 				var ref = this.buttonEntity.split('/')[0],
 				refGuid = this._contextObj.getReference(ref);
-
+				
 				//Listen to the reference selector
 				_contextHandle = this.subscribe({
 						guid: this._contextObj.getGuid(),
 						callback: lang.hitch(this, function (guid) {
 							this._updateButtonTitle();
 						})
-					});
+					});				
 				_refHandle = this.subscribe({
 					guid: this._contextObj.getGuid(),
 					attr: ref,
 					callback: lang.hitch(this, function (guid, attr, attrValue) {
 						this._updateButtonTitle();
 					})
-				});
-
+				});	
+				
 				this._handles = [_refHandle, _contextHandle];
-
+				
 				if(refGuid) {
 					_objectHandle = this.subscribe({
 						guid: refGuid,
 						callback: lang.hitch(this, function (guid) {
 							this._updateButtonTitle();
 						})
-					});
+					});				
 					_attrHandle = this.subscribe({
 						guid: refGuid,
 						attr: this.buttonAttribute,
@@ -446,13 +447,13 @@ define([
 				}
             }
         },
-
-		// mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
-		uninitialize: function() {
-			logger.debug(this.id + ".uninitialize");
+		
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize: function() {
+          	logger.debug(this.id + ".uninitialize");
 			// Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
 			delete this._allDropDowns[this.id];
-		}
+        }
     });
 });
 require(['DropdownDivConverter/widget/DropdownDivConverter'], function () {
